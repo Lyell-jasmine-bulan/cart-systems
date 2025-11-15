81aa79457e1679ef74b6ae0c1056e6cbb53b2850
Lyell-jasmine-bulan <lyellbulan@gmail.com>
2025-11-15 16:07:52 +0800

    cart

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const main = document.querySelector('#main');
+console.log(main)
+
+const article = main.firstElementChild;
+    console.log(article)
+
+
+const h2 = main.firstElementChild;
+console.log(h2);
+
+const section = h2.nextElementSibling;
+console.log(section);
+
+const div = h2.firstElementChild;
+console.log(div);
+
+const p = div.firstElementChild;
+console.log(p)
+
+const p1 = div.nextElementSibling;
+console.log(p1)
+
+const button = div.nextElementSibling;
+console.log(button)
+
+
+
+const div1 =  article.nextElementSibling;
+console.log(div1)
+
+const div2 = article.firstElementChild;
+console.log(div2)
+
+const p3 = div2.firstElementChild;
+console.log(p3)
+
+const p4 = div2.nextElementSibling;
+console.log(p4)
+
+const button1 = p4.firstElementSibling;
+console.log(button1)
+
+
+
+const div3 =  article.nextElementSibling;
+console.log(div3)
+
+const div4 = article.firstElementChild;
+console.log(div4)
+
+const p5 = div4.firstElementChild;
+console.log(p3)
+
+const p6 = div4.nextElementSibling;
+console.log(p4)
+
+const button2 = p6.firstElementSibling;
+console.log(button2)
+
+
+
+const div5 =  article.nextElementSibling;
+console.log(div5)
+
+const div6 = article.firstElementChild;
+console.log(div6)
+
+const p7 = div6.firstElementChild;
+console.log(p7)
+
+const p8 = div6.nextElementSibling;
+console.log(p8)
+
+const button3 = p8.firstElementSibling;
+console.log(button3)
+
+
+
